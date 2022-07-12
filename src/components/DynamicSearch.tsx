9a337a9bf407b4c1c19262763d5b41cc227af54c
James Pickering <james.pickering@gmail.com>
2022-07-12 18:40:51 +0100

    style layout

--- src/components/DynamicSearch.tsx
+++ src/components/DynamicSearch.tsx
@@ -1,8 +1,23 @@
+import React from 'react';
+import { Button } from 'antd';
+import { searchQuery } from '../libs/search';
+
+const search = 'banana';
+
 const DynamicSearch = () => (
-	<div>
-		<label htmlFor="search">Search by name</label>
-		<input name="search" id="search" />
-	</div>
+  <form>
+    <div className='form-group'>
+      <label htmlFor='search'>Search by name</label>
+      <input type='text' className='form-control' name='search' id='search' />
+    </div>
+    <Button
+      type='primary'
+      onClick={() => {
+        searchQuery(search);
+      }}>
+      Submit
+    </Button>
+  </form>
 );
 
 export default DynamicSearch;
--- src/components/FilterForm.tsx
+++ src/components/FilterForm.tsx
@@ -5,23 +5,21 @@ import { searchQuery } from '../libs/search';
 const FilterForm = () => {
   return (
     <form>
-      <div>
-        <span>
-          <label htmlFor='calories-min'>Minimum calories</label>
-          <input name='calories-min' id='calories-min' />
-          <label htmlFor='calories-max'>Maximum calories</label>
-          <input name='calories-max' id='calories-max' />
-        </span>
+      <div className='form-group'>
+        <label htmlFor='calories-min'>Minimum calories</label>
+        <input type='text' name='calories-min' id='calories-min' />
       </div>
-      <div>
-        <Button
-          type='primary'
-          onClick={() => {
-            searchQuery('mango');
-          }}>
-          Fetch data
-        </Button>
+      <div className='form-group'>
+        <label htmlFor='calories-max'>Maximum calories</label>
+        <input type='text' name='calories-max' id='calories-max' />
       </div>
+      <Button
+         type='primary'
+         onClick={() => {
+         searchQuery('all');
+         }}>
+         Fetch data
+      </Button>
     </form>
   );
 };
